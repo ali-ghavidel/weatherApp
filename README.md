3cb1133e8cf3e1fddd8e3a78d414acd7e88bee55
ali-ghavidel <ali-ghavidel@users.noreply.github.com>
2024-02-07 20:25:17 +0000

    deploy: 8a6a290997948ec49cb15052423af6b987eae78b

--- README.md
+++
@@ -1,4 +0,0 @@
-# simple react app for weather
-in this app I used redux & React-Three_fiber
-# Live Demo
-https://sven.ir/weather
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {applyMiddleware, legacy_createStore} from 'redux'
-import weatherReducer from './weather/weatherReducer'
-import { composeWithDevTools } from '@redux-devtools/extension'
-import { thunk } from 'redux-thunk';
-
-const store = legacy_createStore(weatherReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
